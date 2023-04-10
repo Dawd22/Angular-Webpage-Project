d80472c68bed2bcecb04746a67b383476f64567f
Szaniszló Dávid <h164341@stud.u-szeged.hu>
2023-04-10 15:16:15 +0200

    implemented add/update function for room

--- szallashely-project/src/app/pages/admin/admin.component.ts
+++ szallashely-project/src/app/pages/admin/admin.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { room } from 'src/app/shared/models/room';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { RoomService } from 'src/app/shared/services/room.service';
 
 @Component({
@@ -9,18 +12,68 @@ import { RoomService } from 'src/app/shared/services/room.service';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  room: Observable<room[]>;
+  rooms: Observable<room[]>;
+  selectedRoom: room;
+  addRoom: boolean;
 
-  constructor(private roomService: RoomService){}
+  roomForm = new FormGroup({
+    hotel: new FormControl('', Validators.required),
+    type: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    location: new FormGroup({
+      country: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required)
+    })
+
+  });
+
+  constructor(private roomService: RoomService, private afs: AngularFirestore){}
   ngOnInit(): void {
-    this.room = this.roomService.getAll();
+    this.rooms = this.roomService.getAll();
+    const rSub= this.roomService.getAll().subscribe((rooms) => {
+      this.selectedRoom = rooms[0];
+      rSub.unsubscribe;
+    });
+    this.addRoom = false;
   }
-
+  
+  selectRoomById(roomId: string): void {
+    const rSub = this.roomService.getById(roomId).subscribe((room) => {
+      this.selectedRoom = room;
+      console.log(room);
+      rSub.unsubscribe();
+    });
+  }
   delete(roomId: string){
     this.roomService.delete(roomId);
   }
-  add(){}
-  update(){
+  add(): void {
+    const newRoom: room = {
+      id: this.afs.createId(),
+      hotel: this.roomForm.value.hotel,
+      type: this.roomForm.value.type,
+      price: parseInt(this.roomForm.value.price),
+      location: {
+        country: this.roomForm.value.location.country,
+        city: this.roomForm.value.location.city
+      }
+    };
+  
+    this.roomService.create(newRoom).then(() => {
+      this.changeAddRoom();
+    });
+  }
+  
+  
 
+  update() {
+    this.roomService.update(this.selectedRoom).then(() => {
+      alert('Sikeres frissítés!');
+    }).catch((error) => {
+      alert('Hiba történt a frissítés során');
+    });
+  }
+  changeAddRoom(){
+    this.addRoom= !this.addRoom;
   }
 }
--- szallashely-project/src/app/pages/admin/admin.module.ts
+++ szallashely-project/src/app/pages/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { MatButtonModule } from '@angular/material/button';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,13 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     AdminRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class AdminModule { }
